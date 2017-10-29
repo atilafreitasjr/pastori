b22d1efba1905a02ae96f59cc67101adbd94d280
atila <atila@atila-Inspiron-5566>
2017-10-29 06:08:07 -0200

    Versão de dev V 0.1.0
    Funcionando a inserção com layout de tabela

--- class/Historico.php
+++ class/Historico.php
@@ -16,9 +16,51 @@ class Historico {
     private $cod;
     private $data;
     private $numero_animais;
+    private $espectro;
     private $piquete;
     private $mysql;
+    
+    function getData() {
+        return $this->data;
+    }
 
+    function getNumero_animais() {
+        return $this->numero_animais;
+    }
+
+    function getEspectro() {
+        return $this->espectro;
+    }
+
+    function getPiquete() {
+        return $this->piquete;
+    }
+
+    function getMysql() {
+        return $this->mysql;
+    }
+
+    function setData($data) {
+        $this->data = $data;
+    }
+
+    function setNumero_animais($numero_animais) {
+        $this->numero_animais = $numero_animais;
+    }
+
+    function setEspectro($espectro) {
+        $this->espectro = $espectro;
+    }
+
+    function setPiquete($piquete) {
+        $this->piquete = $piquete;
+    }
+
+    function setMysql($mysql) {
+        $this->mysql = $mysql;
+    }
+
+    
     function __construct() {
         $this->mysql = new Conexao;
     }
@@ -43,15 +85,17 @@ class Historico {
         return $array;
     }
 
-    function cadastrar(Historico $classe) {
+    function cadastrar(Historico $classe) {        
         try {
             $this->mysql->sql_query("INSERT INTO `historico` "
-                    . "`data`, "
+                    . "(`data`, "
                     . "`numero_animais`, "
+                    . "`espectro`, "
                     . "`piquete`) "
                     . "VALUES ("
                     . "'" . $classe->data . "', "
-                    . "'" . $classe->numero_animais . "', "
+                    . $classe->numero_animais . ", "
+                    . "'" . $classe->espectro . "', "
                     . "'" . $classe->piquete . "')");
             $this->msg['txt'] = "<h4 class = 'txt_sucesso_form'>Registro gravado com sucesso!</h4><br>";
             $this->msg['status'] = true;
@@ -60,6 +104,15 @@ class Historico {
             echo $exc->getTraceAsString();
         }
         return $this->msg;
+    }
+    
+    function set_imputs($imputs) {
+        
+        $this->setData(date("Y-m-d"));
+        empty($imputs['numero_animais']) ? $this->setNumero_animais(0) : $this->setNumero_animais($imputs['numero_animais']);
+        empty($imputs['espectro']) ? $this->setEspectro('') : $this->setEspectro($imputs['espectro']);
+        empty($imputs['piquete']) ? $this->setPiquete('') : $this->setPiquete($imputs['piquete']);
+        return $this;
     }
 
 }
--- index.php
+++ index.php
@@ -1,43 +1,55 @@
 <?php
 session_start();
+require_once 'class/Conexao.php';
+require_once 'class/Historico.php';
+
+$historico = new Historico();
+
+$dados = $historico->listar();
+
+
 $imputs = filter_input_array(INPUT_POST, $_POST);
 
-isset($imputs['piquete']) ? '' : $imputs['piquete'] = '1';
+
+if (isset($imputs['piquete'])) {
+    $inserir = $historico->set_imputs($imputs);
+    $historico->cadastrar($inserir);
+} else {
+    $imputs['piquete'] = '1';
+}
 
 for ($index = 1; $index < 9; $index++) {
-    
+
     $mensagem['g'][$index] = '';
     $mensagem['r'][$index] = '';
-    
-    if (!isset($_SESSION['r'][$index])) {        
+
+    if (!isset($_SESSION['r'][$index])) {
         $_SESSION['r'][$index] = 100;
-       
     }
     if (!isset($_SESSION['g'][$index])) {
         $_SESSION['g'][$index] = 100;
-       
     }
-    
+
     if ($index == $imputs['piquete']) {
-        if($_SESSION['r'][$index] > 250){
+        if ($_SESSION['r'][$index] > 250) {
             $_SESSION['r'][$index] = 251;
             $mensagem['r'][$index] = 'Troque de piquete';
         } else {
             $_SESSION['r'][$index] = $_SESSION['r'][$index] + 30;
         }
-        if ($_SESSION['g'][$index]< 10) {
+        if ($_SESSION['g'][$index] < 10) {
             $_SESSION['g'][$index] = 10;
         } else {
             $_SESSION['g'][$index] = $_SESSION['g'][$index] - 10;
         }
         $b[$index] = random_int(50, 200);
     } else {
-        if($_SESSION['r'][$index] < 10){
+        if ($_SESSION['r'][$index] < 10) {
             $_SESSION['r'][$index] = 9;
         } else {
             $_SESSION['r'][$index] = $_SESSION['r'][$index] - 30;
         }
-        if ($_SESSION['g'][$index] > 250) {            
+        if ($_SESSION['g'][$index] > 250) {
             $_SESSION['g'][$index] = 250;
         } else {
             $_SESSION['g'][$index] = $_SESSION['g'][$index] + 10;
@@ -46,16 +58,24 @@ for ($index = 1; $index < 9; $index++) {
     }
     $cor[$index] = $_SESSION['r'][$index] . ',' . $_SESSION['g'][$index] . ',' . $b[$index];
 }
-echo'<br>$index<pre>';
-print_r($cor);
-echo '</pre><br>';
 ?>
 
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <title>css demo</title>
+        <meta charset="UTF-8">
+	<title>Tablesorter - Tutsup</title>
+	
+	<!-- Estilos necessários para o tema do tablesorter -->
+	<link rel="stylesheet" href="css/blue/style.css">
+	
+	<!-- jQuery e Tablesorter -->
+	<script src="js/jquery-latest.js"></script>
+	<script src="js/jquery.tablesorter.min.js"></script>
+	
+	<!-- Meu script -->
+	<script src="js/scripts.js"></script>
+        <title>Pratori</title>
         <style>
             div {
                 width: 100px;
@@ -68,22 +88,61 @@ echo '</pre><br>';
     </head>
     <body>
 
-        <span id="result">&nbsp;</span>
-        <div style="background-color:rgb(<?php echo $cor['1']; ?>)"><?= $mensagem['r'][1] ?><br>1</div>
-        <div style="background-color:rgb(<?php echo $cor['2']; ?>)"><?= $mensagem['r'][2] ?><br>2</div>
-        <div style="background-color:rgb(<?php echo $cor['3']; ?>)"><?= $mensagem['r'][3] ?><br>3</div>
-        <div style="background-color:rgb(<?php echo $cor['4']; ?>)"><?= $mensagem['r'][4] ?><br>4</div>
-        <div style="background-color:rgb(<?php echo $cor['5']; ?>)"><?= $mensagem['r'][5] ?><br>5</div>
-        <div style="background-color:rgb(<?php echo $cor['6']; ?>)"><?= $mensagem['r'][6] ?><br>6</div>
-        <div style="background-color:rgb(<?php echo $cor['7']; ?>)"><?= $mensagem['r'][7] ?><br>7</div>
-        <div style="background-color:rgb(<?php echo $cor['8']; ?>)"><?= $mensagem['r'][8] ?><br>8</div>
-        <br>
-        <br>
+        <table border="0">
+            <thead>
+                <tr>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div style="background-color:rgb(<?php echo $cor['1']; ?>)"><?= $mensagem['r'][1] ?><br>1</div>
+                        <div style="background-color:rgb(<?php echo $cor['2']; ?>)"><?= $mensagem['r'][2] ?><br>2</div>
+                        <div style="background-color:rgb(<?php echo $cor['3']; ?>)"><?= $mensagem['r'][3] ?><br>3</div>
+                        <div style="background-color:rgb(<?php echo $cor['4']; ?>)"><?= $mensagem['r'][4] ?><br>4</div>
+                        <div style="background-color:rgb(<?php echo $cor['5']; ?>)"><?= $mensagem['r'][5] ?><br>5</div>
+                        <div style="background-color:rgb(<?php echo $cor['6']; ?>)"><?= $mensagem['r'][6] ?><br>6</div>
+                        <div style="background-color:rgb(<?php echo $cor['7']; ?>)"><?= $mensagem['r'][7] ?><br>7</div>
+                        <div style="background-color:rgb(<?php echo $cor['8']; ?>)"><?= $mensagem['r'][8] ?><br>8</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <form action="#" method="POST">
+                            <input hidden="on" type="text" name="espectro" value="<?= $cor[$imputs['piquete']] ?>" />
+                            Onde estão os animais:<input type="text" width="2" name="piquete" value="<?= $imputs['piquete'] ?>" />    
+                            <input type="submit" value="Atualizar dado do Drone" name="drone" />
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
 
-        <form action="#" method="POST">
-            Onde estão os animais:<input type="text" name="piquete" value="<?= $imputs['piquete'] ?>" />    
-            <input type="submit" value="Atualizar dado do Drone" name="drone" />
-        </form>
-        <br>
+                        <table border="1" class="tablesorter">
+                            <thead>
+                                <tr>
+                                    <th>código</th>
+                                    <th>piquete</th>
+                                    <th>espectro de cor</th>
+                                    <th>data</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($dados as $value) { ?>
+                                    <tr>
+
+                                        <td><?= $value->cod ?></td>
+                                        <td><?= $value->piquete ?></td>
+                                        <td><?= $value->espectro ?></td>
+                                        <td><?= $value->data ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </body>
 </html>
